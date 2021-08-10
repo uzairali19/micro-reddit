d3cd1dd9bec0a812a091662f02a8d7f3c544104c
Uzair <uzairali19b@gmail.com>
2021-08-10 19:02:32 +0500

    fix linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ApplicationRecord
-    validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
+  validates :username, presence: true, length: { in: 4..12 }, uniqueness: true
 end
